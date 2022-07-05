e852158f9093905874dc76de68bdb0e822e7f8f0
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-07-05 14:34:13 -0400

    fixed iframe issue

--- pages/index.js
+++ pages/index.js
@@ -61,13 +61,13 @@ export default function Index() {
           <iframe
             title="Delaine Abner - Resume (3)"
             src="https://www.scribd.com/embeds/581278965/content?start_page=1&view_mode=scroll&access_key=key-DVAWH5v61sAVMZD3L2eT"
-            tabindex="0"
+            tabIndex="0"
             data-auto-height="true"
             data-aspect-ratio="0.7729220222793488"
             scrolling="no"
             width="100%"
             height="600"
-            frameborder="0"
+            frameBorder="0"
           ></iframe>
         
         </div>
